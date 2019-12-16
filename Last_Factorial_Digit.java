d7631ee52519813c7aaa83e927de53c8761c5e43
Maezkev <58494367+Maezkev@users.noreply.github.com>
2019-12-16 22:23:40 +0700

    Add files via upload

---
+++ Last_Factorial_Digit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class triv {
+  public static void main(String[] args) {
+    Scanner inputuser = new Scanner(System.in);
+    int much = inputuser.nextInt();
+    int [] inpt = new int[much+1];
+    int [] jwb = new int[much+1];
+    for(int i = 1 ; i <= much ; i++){
+      inpt[i] = inputuser.nextInt();
+    }
+    for(int j = 1 ; j <= much ; j++){
+      jwb[j] = 1;
+      for(int k = 1 ; k <= inpt[j] ; k++){
+        jwb[j] *= k;
+      }
+      System.out.println(jwb[j] % 10); //last digit (% 10), first digit (/10)
+    }
+  }   
+}
